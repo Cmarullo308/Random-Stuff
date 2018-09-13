c2d7050d746e857c1d8f64e2db54a5ac47fef09a
CJ <Ershkabible94@gmail.com>
2018-09-13 18:17:13 -0400

    Main Scene Mostly Done
    Starting to add functions

--- src/RandomStuff/Main.java
+++ src/RandomStuff/Main.java
@@ -1,9 +1,62 @@
-package RandomStuff;
+package randomStuff;
+
+import java.io.IOException;
 
 import Tools.Print;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+	private static Stage primaryStage;
+	private static BorderPane mainLayout;
+	static FXMLLoader loader;
+
 	public static void main(String[] args) {
-		Print.pl("Test");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Random Stuff");
+		showMainView();
+		// setOptionsBoxView(); // delete
+	}
+
+	private void showMainView() {
+		loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+		try {
+			mainLayout = loader.load();
+		} catch (IOException e) {
+			Print.pl("thrown from \"showMainView\" in Main");
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	/**
+	 * Sets up the options after clicking a lebel in the inner list
+	 * @param optionsWindow
+	 * @param selectedItemFromList
+	 * @throws IOException 
+	 */
+	public static void setOptionsBoxView(BorderPane optionsWindow, String resourcePath) throws IOException {
+		loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource(resourcePath));
+		BorderPane mainItems = loader.load();
+		
+		optionsWindow.setCenter(mainItems);
+	}
+
+	public static void quitProgram() {
+		System.exit(0);
+		return;
 	}
 }
---
+++ src/randomStuff/View/MainViewController.java
@@ -0,0 +1,87 @@
+package randomStuff.View;
+
+import java.io.IOException;
+
+import Tools.Print;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import randomStuff.Main;
+
+//To change innerList options go to handleFirstListChoice()
+
+public class MainViewController {
+	Main main;
+
+	ObservableList<String> outerListItems = FXCollections.observableArrayList("Text", "Option 2");
+	ObservableList<String> innerListItems;
+	// Text Options
+
+	@FXML
+	private ListView<String> outerList;
+	@FXML
+	private ListView<String> innerList;
+	@FXML
+	private BorderPane OptionsWindow;
+
+	// Menu bar - File
+	@FXML
+	private MenuItem exit;
+
+	@FXML
+	private void initialize() {
+		//OuterList
+		outerList.setItems(outerListItems);
+		outerList.getSelectionModel().select(0);
+		handleFirstListChoice();
+	}
+
+	
+	/**
+	 * Called when an option from the outter list is selected
+	 */
+	@FXML
+	private void handleFirstListChoice() {
+		String selectedList = outerList.getSelectionModel().getSelectedItem();
+
+		if (selectedList.equals("Text")) {
+			innerListItems = FXCollections.observableArrayList("Reverse Text");
+		} else if (selectedList.equals("Option 2")) {
+			innerListItems = FXCollections.observableArrayList("Test");
+		}
+
+		innerList.setItems(innerListItems);
+	}
+
+	/**
+	 * Called when an option from the inner list is selected
+	 * @throws IOException
+	 */
+	@FXML
+	private void handleSecondListChoice() throws IOException {
+		// Print.pl("handleSecondChoice called");
+		changeOptionBoxView(innerList.getSelectionModel().getSelectedItem());
+	}
+
+	@FXML
+	private void changeOptionBoxView(String selectedItemFromList) throws IOException {
+		String resourcePath = "";
+
+		if (selectedItemFromList.equals("Reverse Text")) {
+			resourcePath = "View/Text/ReverseText/ReverseTextView.fxml";
+		} else {
+			return;
+		}
+
+		Main.setOptionsBoxView(OptionsWindow, resourcePath);
+	}
+
+	@FXML // Calls the quitProgram function in Main
+	private void exitButton() {
+		Main.quitProgram();
+	}
+}
